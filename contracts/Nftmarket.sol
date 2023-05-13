5ceacf59787e8f0e6fe1fa090112c9c23a02faa2
pallavisaxena215 <pallavisaxena215@gmail.com>
2023-05-13 15:54:11 +0530

    first commit

---
+++ contracts/Nftmarket.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.5.0 <=0.9.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+
+contract Nftmarket {
+    using Counters for Counters.Counter;
+   address payable public owner;
+
+   struct TokenDetail {
+    uint id;
+    uint price;
+    address payable owner;
+    address payable seller;
+    bool exists;
+
+   }
+
+   event TokenCreated(
+    uint id,
+    uint price,
+    address payable owner,
+    address payable seller,
+    bool exists
+
+   );
+
+   mapping(uint=>TokenDetail) TokenDetails;
+//    constructor(){
+//     owner = payable(msg.sender);
+//    }
+
+}
