d262ae144869c05b79593371d14870640e387aa8
pallavisaxena215 <pallavisaxena215@gmail.com>
2023-05-14 12:57:07 +0530

    added contract functions

--- contracts/Nftmarket.sol
+++ contracts/Nftmarket.sol
@@ -5,13 +5,15 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
 
-contract Nftmarket {
+contract Nftmarket is ERC721{
     using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
    address payable public owner;
-
+   uint tax = 2;
    struct TokenDetail {
     uint id;
     uint price;
+    string tokenURI;
     address payable owner;
     address payable seller;
     bool exists;
@@ -24,15 +26,42 @@ contract Nftmarket {
     address payable owner,
     address payable seller,
     bool exists
-
    );
 
-   mapping(uint=>TokenDetail) TokenDetails;
-   Counters.Counter private _tokengen;
-   Counters.Counter private _tokensold;
+   modifier onlyOwner(){
+    require(owner == msg.sender,"Only pwner of the smart contract can call this function");
+    _;
+   }
 
-//    constructor(){
-//     owner = payable(msg.sender);
-//    }
+   mapping(uint=>TokenDetail) TokenDetails;
+   
+
+   constructor() ERC721("NFT Token","NFTT"){
+    owner = payable(msg.sender);
+   }
+
+   function updateTax(uint _tax) public payable onlyOwner{
+    tax = _tax;
+   }
+
+  function createToken(string memory _tokenURI,uint _price) public payable returns (uint){
+     
+     _tokenIds.increment();
+
+     TokenDetail storage newToken = TokenDetails[_tokenIds];
+     newToken.owner = owner;
+     newToken.id = _tokenIds;
+     newToken.price= _price;
+     newToken.exists=true;
+     newToken.tokenURI=_tokenURI;
+     newToken.seller = owner;
+  }
+
+
+  function buyToken(uint _tokenId) public payable returns (uint){
+     require(msg.value >= TokenDetails[_tokenId],"The amount is less than the token price");
+     newToken.owner = payable(msg.sender);
+     newToken.seller = payable(msg.sender);
+  }
 
 }
