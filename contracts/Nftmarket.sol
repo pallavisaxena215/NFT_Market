a7f4a3ce6f67f9450e6b92e09951d6c344193aea
pallavisaxena215 <pallavisaxena215@gmail.com>
2023-05-15 14:36:31 +0530

    added sell func

--- contracts/Nftmarket.sol
+++ contracts/Nftmarket.sol
@@ -9,6 +9,7 @@ import "hardhat/console.sol";
 contract Nftmarket is ERC721URIStorage{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+     Counters.Counter private _tokenSold;
    address payable public owner;
    uint tax = 2;
    struct TokenDetail {
@@ -56,21 +57,34 @@ contract Nftmarket is ERC721URIStorage{
     return temp_id;
   }
 
-  function createToken(uint _tokenId,uint _price) public payable returns (uint){
+  function createToken(uint _tokenId,uint _price) public payable{
     require(_price>0, "Price must be greater than zero");
     require(msg.value == tax,"Please pay the listing price");
     TokenDetails[_tokenId] = TokenDetail(
     _tokenId,
     _price,
-     payable(address(this)),
+    payable(address(this)),
     payable(msg.sender),
     true
     );
 
 
      _transfer(msg.sender, address(this), _tokenId);
+     owner.transfer(tax);
 
   }
+
+function sellToken(uint _tokenId) public payable{
+    require(msg.sender == TokenDetails[_tokenId].owner, "Only owner of the token can call the function");
+    require(TokenDetails[_tokenId].price > msg.value,"Please pay the purchasing price");
+    _transfer(TokenDetails[_tokenId].owner,msg.sender, _tokenId);
+    TokenDetails[_tokenId].owner = payable(msg.sender);
+    TokenDetails[_tokenId].exists = false;
+    _tokenSold.increment();
+    TokenDetails[_tokenId].seller.transfer(msg.value);
+
+  }
+
 
 
 }
