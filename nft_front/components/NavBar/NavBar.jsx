ec939e5ef6f4ee6995ff750c93e8c410194d4aea
pallavisaxena215 <pallavisaxena215@gmail.com>
2023-05-20 11:56:31 +0530

    frontend init

--- nft_front/components/NavBar/NavBar.jsx
+++ nft_front/components/NavBar/NavBar.jsx
@@ -1,36 +1,9 @@
-import React from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
-import styles from './NavBar.module.css';
+import React from 'react'
 
-const NavBar = ({ children }) => {
+const NavBar = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Next.js Website</title>
-        <link rel="stylesheet" href="/global.css" />
-      </Head>
-      <nav className={styles.navbar}>
-        <ul className={styles.navbarMenu}>
-          <li className={styles.navbarItem}>
-           
-          </li>
-          <li className={styles.navbarItem}>
-         
-          </li>
-        </ul>
-      </nav>
-      <div className={styles.background}>
-        <header className={styles.header}>
-          <h1>Next.js Website</h1>
-        </header>
-        <main className={styles.content}>{children}</main>
-        <footer className={styles.footer}>
-          <p>&copy; 2023 Next.js Website. All rights reserved.</p>
-        </footer>
-      </div>
-    </div>
-  );
-};
+    <div>NavBar</div>
+  )
+}
 
-export default NavBar;
+export default NavBar
---
+++ nft_front/components/NavBar/index.js
@@ -0,0 +1,7 @@
+import Discover from "./Discover/Discover";
+import Help from "./Help/Help";
+import Profile from "./Profile/Profile";
+import Notification from "./Notification/Notification";
+import SideBar from "./SideBar/SideBar";
+
+export { Discover, HelpCenter, Profile, Notification, SideBar };
